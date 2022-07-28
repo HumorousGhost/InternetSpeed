b404b1ac41624fdcf17b9ed4951eff13e722a075
v-zhangliqun <v-zhangliqun@360.cn>
2022-07-28 16:42:17 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# InternetSpeed
+
+A description of this package.
---
+++ Tests/InternetSpeedTests/InternetSpeedTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import InternetSpeed
+
+final class InternetSpeedTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(InternetSpeed().text, "Hello, World!")
+    }
+}
